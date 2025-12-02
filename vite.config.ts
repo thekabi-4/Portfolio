53c6f2d647da07994a81bc85185d1dda6c2ecae0
kabi <kabileshmva@gmail.com>
2025-12-02 20:04:51 +0530

    theme

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, ".", "");
+
+  const isGithub = env.VITE_ENV === "github";
+
   return {
-    base: "/Portfolio/",
+    base: isGithub ? "/Portfolio/" : "/",
     server: {
       port: 3000,
       host: "0.0.0.0",
